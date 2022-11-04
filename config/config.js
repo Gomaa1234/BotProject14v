1919de5af1549aee43e2907203e03978d0535c92
GOMAA123 <73722597+Gomaa1234@users.noreply.github.com>
2022-11-04 12:37:36 +0000

    new bot code

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  Prefix: "?", // YOUR BOT PREFIX.
+
+  Users: {
+    OWNERS: ["849413565487382578", "554029264492691476","403257716925005826"] // THE BOT OWNERS ID.
+  },
+
+  Handlers: {
+    MONGO: "mongodb+srv://GOMAA:HLVssXOEsY3MoU6F@gcode.8yb6s5b.mongodb.net/?retryWrites=true&w=majority" // YOUR MONGO URI. (USE THIS ONLY IN VSCODE)
+  },
+
+  Client: {
+    TOKEN: "OTA5MjE5MDc4MjUyNjgzMjk1.G71-W0.xyARiF6pzuvs2PJaEG88K0nXPNIZKucH4RuP8o", // YOUR BOT TOKEN. (USE THIS ONLY IN VSCODE)
+    ID: "909219078252683295" // YOUR BOT ID.
+  }
+
+}
